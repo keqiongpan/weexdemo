a10ac4e5cb2f99ebfdbe32bfd6ed9b8447feb2cf
Qiongpan Ke <keqiongpan@163.com>
2019-04-21 20:03:06 +0800

    [新增] 为SwitchRadio开关按钮组件添加动画效果，动画效果使用CSS3中的transition样式进行处理，通过切换样式进行触发，可以支持Web/Android/iOS三端，也是最简单的一种。但由于Weex的计算问题，在Android/iOS上的位置计算可能会有误差。

--- weexdemo/src/components/SwitchRadio.vue
+++ weexdemo/src/components/SwitchRadio.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="switch-radio" @click="onClick">
+  <div class="switch-radio" @click="value = !value">
     <div ref="track" class="track" :class="[value ? 'track-on' : 'track-off']"></div>
     <div ref="groove" class="groove" :class="[value ? 'groove-on' : 'groove-off']"></div>
     <div ref="thumb" class="thumb" :class="[value ? 'thumb-on' : 'thumb-off']"></div>
@@ -7,7 +7,6 @@
 </template>
 
 <script>
-const animation = weex.requireModule('animation')
 export default {
   props: ['on'],
   data: function () {
@@ -18,45 +17,6 @@ export default {
   watch: {
     value: function (newValue, oldValue) {
       this.$emit('update:on', newValue)
-    }
-  },
-  methods: {
-    animation: function (value, done) {
-      let vm = this
-      let animationCount = 0
-
-      let animationDone = function () {
-        if (--animationCount === 0) {
-          done()
-        }
-      }
-
-      let animationProcess = function () {
-        animationCount++
-        animation.transition(vm.$refs.track, {
-          styles: { backgroundColor: `${value ? '#4cd864' : '#e5e5e5'}` },
-          duration: 150
-        }, animationDone)
-
-        animationCount++
-        animation.transition(vm.$refs.groove, {
-          styles: { transform: `scale(${value ? 0 : 1})` },
-          duration: 150
-        }, animationDone)
-
-        animationCount++
-        animation.transition(vm.$refs.thumb, {
-          styles: { transform: `translateX(${value ? 40 : 0}px)` },
-          duration: 150
-        }, animationDone)
-      }
-
-      setTimeout(animationProcess, 0)
-    },
-    onClick: function () {
-      let vm = this
-      let value = !vm.value
-      vm.animation(value, function () { vm.value = value })
     }
   }
 }
@@ -74,9 +34,13 @@ export default {
   }
   .track-on {
     background-color: #4cd864;
+    transition-property: background-color;
+    transition-duration: 0.15s;
   }
   .track-off {
     background-color: #e5e5e5;
+    transition-property: background-color;
+    transition-duration: 0.15s;
   }
   .groove {
     position: absolute;
@@ -89,9 +53,13 @@ export default {
   }
   .groove-on {
     transform: scale(0);
+    transition-property: transform;
+    transition-duration: 0.15s;
   }
   .groove-off {
     transform: scale(1);
+    transition-property: transform;
+    transition-duration: 0.15s;
   }
   .thumb {
     position: absolute;
@@ -105,8 +73,12 @@ export default {
   }
   .thumb-on {
     transform: translateX(40px);
+    transition-property: transform;
+    transition-duration: 0.15s;
   }
   .thumb-off {
     transform: translateX(0px);
+    transition-property: transform;
+    transition-duration: 0.15s;
   }
 </style>
